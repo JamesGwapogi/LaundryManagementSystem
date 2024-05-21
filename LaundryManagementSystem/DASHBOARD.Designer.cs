11256bb89f2b0ef1938e89e9f5c7e70cb8d8a9b4
Jamesogood <carrascalmarissa5@gmail.com>
2024-05-21 18:38:03 +0800

    Add project files.

---
+++ LaundryManagementSystem/DASHBOARD.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LaundryManagementSystem
+{
+    partial class DASHBOARD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DASHBOARD));
+            this.buttonMenu = new System.Windows.Forms.Button();
+            this.buttonProfile = new System.Windows.Forms.Button();
+            this.buttonCart = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMenu
+            // 
+            this.buttonMenu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonMenu.BackgroundImage")));
+            this.buttonMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenu.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonMenu.Location = new System.Drawing.Point(35, 48);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(48, 47);
+            this.buttonMenu.TabIndex = 0;
+            this.buttonMenu.UseVisualStyleBackColor = true;
+            // 
+            // buttonProfile
+            // 
+            this.buttonProfile.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonProfile.BackgroundImage")));
+            this.buttonProfile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonProfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProfile.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonProfile.Location = new System.Drawing.Point(35, 101);
+            this.buttonProfile.Name = "buttonProfile";
+            this.buttonProfile.Size = new System.Drawing.Size(48, 49);
+            this.buttonProfile.TabIndex = 1;
+            this.buttonProfile.UseVisualStyleBackColor = true;
+            // 
+            // buttonCart
+            // 
+            this.buttonCart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonCart.BackgroundImage")));
+            this.buttonCart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCart.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonCart.Location = new System.Drawing.Point(35, 156);
+            this.buttonCart.Name = "buttonCart";
+            this.buttonCart.Size = new System.Drawing.Size(48, 49);
+            this.buttonCart.TabIndex = 2;
+            this.buttonCart.UseVisualStyleBackColor = true;
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonSettings.BackgroundImage")));
+            this.buttonSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSettings.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonSettings.Location = new System.Drawing.Point(35, 211);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(48, 49);
+            this.buttonSettings.TabIndex = 3;
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(35, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 49);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DASHBOARD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.buttonCart);
+            this.Controls.Add(this.buttonProfile);
+            this.Controls.Add(this.buttonMenu);
+            this.Name = "DASHBOARD";
+            this.Text = "DASHBOARD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMenu;
+        private System.Windows.Forms.Button buttonProfile;
+        private System.Windows.Forms.Button buttonCart;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Button button1;
+    }
+}
